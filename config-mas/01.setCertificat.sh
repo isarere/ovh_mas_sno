cc8a61b78dd8ed2f21ce7e10f684e2726049c8f7
Wei HUANG <wei.b.huang@capgemini.com>
2024-06-22 17:02:56 +0200

    update certificate of MAS

--- config-mas/01.setCertificat.sh
+++ config-mas/01.setCertificat.sh
@@ -11,11 +11,15 @@ PK_PATH=$2
 CA_PATH=$3
 PUB_PATH=$4
 
-oc project mas-$INST_NAME-core
 # Activate manual certificate
 oc patch suite/$INST_NAME -n mas-$INST_NAME-core --type='json' -p='[{"op": "replace", "path": "/spec/settings/manualCertMgmt", "value":True}]'
 
-# update certificates
+# update certificates pour mas core
 oc set data secret/$INST_NAME-cert-public -n mas-$INST_NAME-core --from-file=tls.key=$PK_PATH
 oc set data secret/$INST_NAME-cert-public -n mas-$INST_NAME-core --from-file=ca.crt=$CA_PATH
 oc set data secret/$INST_NAME-cert-public -n mas-$INST_NAME-core --from-file=tls.crt=$PUB_PATH
+
+# update certificates pour mas manage
+oc set data secret/$INST_NAME-mas-cert-public-81 -n mas-$INST_NAME-manage --from-file=tls.key=$PK_PATH
+oc set data secret/$INST_NAME-mas-cert-public-81 -n mas-$INST_NAME-manage --from-file=ca.crt=$CA_PATH
+oc set data secret/$INST_NAME-mas-cert-public-81 -n mas-$INST_NAME-manage --from-file=tls.crt=$PUB_PATH
